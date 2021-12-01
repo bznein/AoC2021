6ffd3540aecba01689924040cab0efef845096af
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2021-12-01 08:42:31 +0000

    refactor

--- pkg/Day01/main.go
+++ pkg/Day01/main.go
@@ -12,21 +12,20 @@ func Solve(inputF string) (int, int) {
 	part1 := 0
 	part2 := 0
 	ints := input.InputToIntSlice(inputF)
+	curWindowSum := ints[2] + ints[1] + ints[0]
+
 	for i := 1; i < len(ints); i++ {
 		if ints[i] > ints[i-1] {
 			part1++
 		}
-	}
-
-	sums := []int{}
-	for i := 2; i < len(ints); i++ {
-		sums = append(sums, ints[i]+ints[i-1]+ints[i-2])
-	}
-
-	for i := 1; i < len(sums); i++ {
-		if sums[i] > sums[i-1] {
-			part2++
+		if i > 2 {
+			tempSum := ints[i] + ints[i-1] + ints[i-2]
+			if tempSum > curWindowSum {
+				part2++
+			}
+			curWindowSum = tempSum
 		}
 	}
+
 	return part1, part2
 }
